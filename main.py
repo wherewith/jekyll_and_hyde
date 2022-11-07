ac041f3497e814c82af7edc7a758949d56d66de6
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2022-11-07 16:16:53 -0600

    update with functions to run as either user

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ openai.api_key = OPENAI_TOKEN
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to: ')
+    string = f'{client.user} has connected to: '
+    print("#" * len(string))
+    print(string)
     print(', '.join(map(str, client.guilds)))
+    print("#" * len(string))
+    await client.user.edit(username="Dr. Jekyll and Mr. Hyde")
 
 @client.event
 async def on_error(event, *args, **kwargs):
@@ -27,24 +31,28 @@ async def on_message(message):
     if message.author == client.user: # if the bot sends a message, ignore it
         return
     if "!openai" in message.content[0:7]:
-        prompt = message.content[message.content.index("!openai")+8:]
-        print(prompt)
-        completion = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            temperature=0.7,
-            max_tokens=256,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0)
-        func = message.channel.send(completion["choices"][0]["text"].lstrip())
-        await run_as_jekyll(func)
+        await run_openai(message)
+
+async def run_openai(message):
+    prompt = message.content[message.content.index("!openai") + 8:]
+    completion = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0)
+    func = message.channel.send(completion["choices"][0]["text"].lstrip())
+    await run_as_jekyll(func)
 
 async def run_as_jekyll(func):
     await client.user.edit(username="Dr. Jekyll")
     await func
+    await client.user.edit(username="Dr. Jekyll and Mr. Hyde")
 async def run_as_hyde(func):
     await client.user.edit(username="Mr. Hyde")
     await func
+    await client.user.edit(username="Dr. Jekyll and Mr. Hyde")
 
 client.run(DISCORD_TOKEN)
