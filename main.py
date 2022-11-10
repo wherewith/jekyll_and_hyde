83bb1400bb4a6aa9e859999ae2627dc4c693df54
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2022-11-10 11:47:46 -0600

    improved modularity

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 OPENAI_TOKEN = os.getenv('OPENAI_TOKEN')
 client = discord.Client(intents=discord.Intents.all())
 openai.api_key = OPENAI_TOKEN
+default_username = "Dr. Jekyll and Mr. Hyde"
 
 @client.event
 async def on_ready():
-    string = f'{client.user} has connected to: '
-    print("#" * len(string))
-    print(string)
+    connection_output = f'{client.user} has connected to: '
+    print("#" * len(connection_output))
+    print(connection_output)
     print(', '.join(map(str, client.guilds)))
-    print("#" * len(string))
-    await client.user.edit(username="Dr. Jekyll and Mr. Hyde")
+    print("#" * len(connection_output))
+    await client.user.edit(username=default_username)
 
 @client.event
 async def on_error(event, *args, **kwargs):
@@ -30,11 +31,14 @@ async def on_error(event, *args, **kwargs):
 async def on_message(message):
     if message.author == client.user: # if the bot sends a message, ignore it
         return
-    if "!openai" in message.content[0:7]:
-        await ask_openai(message)
+    if prefix not in message.content[0]:
+        return
+    for command in functions:
+        if command in message.content[1:len(command)+1]:
+            await functions.get(command)(message, command)
 
-async def ask_openai(message):
-    prompt = message.content[message.content.index("!openai") + 8:]
+async def ask_openai(message, command):
+    prompt = message.content[(message.content.index(prefix) + len(command)):]
     completion = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -49,10 +53,13 @@ async def ask_openai(message):
 async def run_as_jekyll(func):
     await client.user.edit(username="Dr. Jekyll")
     await func
-    await client.user.edit(username="Dr. Jekyll and Mr. Hyde")
+    await client.user.edit(username=default_username)
 async def run_as_hyde(func):
     await client.user.edit(username="Mr. Hyde")
     await func
-    await client.user.edit(username="Dr. Jekyll and Mr. Hyde")
+    await client.user.edit(username=default_username)
+
+prefix = "!"
+functions = {"openai": ask_openai}
 
 client.run(DISCORD_TOKEN)
